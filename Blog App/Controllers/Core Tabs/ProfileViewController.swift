5db1b3e27b796774f513603cdc2f48cfd4f1dd60
kenig39 <96082144+kenig39@users.noreply.github.com>
2023-09-02 12:24:53 +0300

    add tableview

--- Blog App/Controllers/Core Tabs/ProfileViewController.swift
+++ Blog App/Controllers/Core Tabs/ProfileViewController.swift
@@ -2,18 +2,58 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+    
+    
+    
+    let currentEmail: String
+    
+    init(currentEmail: String) {
+        self.currentEmail = currentEmail
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableView.self, forCellReuseIdentifier: "cell")
+       return tableView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
         view.backgroundColor = .systemBackground
+        setUpSignOutButton()
+       setUpTableView()
+      
+    }
+    
+    private func setUpTableView(){
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    
+    private func setUpSignOutButton(){
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Sigh Out",
                                                             style: .done,
                                                             target: self, action: #selector(didTapSignOut))
     }
     
+    
+    
+    // Sing Out
     @objc private func  didTapSignOut(){
         let sheet = UIAlertController(title: "Sign Out", message: "Are you shure to go away", preferredStyle: .actionSheet)
         sheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
@@ -38,7 +78,18 @@ class ProfileViewController: UIViewController {
         present(sheet, animated: true)
     }
     
+    //TableView
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Blog post goes"
+        
+        return cell
+    }
   
 
 }
