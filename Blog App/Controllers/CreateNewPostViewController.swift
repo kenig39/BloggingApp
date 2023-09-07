adb3afa6a68362b31d18b384b2170cb8f6e23c87
kenig39 <96082144+kenig39@users.noreply.github.com>
2023-09-07 23:00:54 +0300

    add image view

--- Blog App/Controllers/CreateNewPostViewController.swift
+++ Blog App/Controllers/CreateNewPostViewController.swift
@@ -8,16 +8,43 @@
 import UIKit
 
 class CreateNewPostViewController: UITabBarController {
-
+    
+    //Titlr to field
+    
+    
+    //Image Header
+    private let headerImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.isUserInteractionEnabled = true
+        imageView.image = UIImage(systemName: "photo")
+        imageView.backgroundColor = .tertiarySystemBackground
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        configureButton()
+    }
+    
+    
+    private  func configureButton(){
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Cancel",
                                                            style: .done,
                                                            target: self,
                                                            action: #selector(didTapCancel))
-       
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post", style: .done, target: self, action: #selector(didTapPost))
+        
     }
     
-
+    @objc func didTapCancel(){
+        
+    }
+    
+    @objc func didTapPost(){
+        
+    }
+        
 }
