2286647bed6c08bd394f2f3a88d0876cabab5000
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-01-23 22:36:12 +0300

    add some code

--- Blog App/Views/Cells/PostPreviewTableViewTableViewCell.swift
+++ Blog App/Views/Cells/PostPreviewTableViewTableViewCell.swift
@@ -11,9 +11,26 @@ class PostPreviewTableViewTableViewCell: UITableViewCell {
 
     static let indentifire = "PostPreviewTableViewCell"
     
+    private let postImageView: UIImageView = {
+    let imageView = UIImageView()
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 8
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let postTitleLabel: UILabel = {
+       let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 20, weight: .medium)
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.clipsToBounds = true
+        contentView.addSubview(postImageView)
+        contentView.addSubview(postTitleLabel)
     }
     
     required init?(coder: NSCoder) {
