8be4dd2b2c861fab1dd541b852a18348296029ab
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-01-23 22:30:40 +0300

    add some code

--- Blog App/Views/Cells/PostPreviewTableViewTableViewCell.swift
+++ Blog App/Views/Cells/PostPreviewTableViewTableViewCell.swift
@@ -9,5 +9,26 @@ import UIKit
 
 class PostPreviewTableViewTableViewCell: UITableViewCell {
 
-  
+    static let indentifire = "PostPreviewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.clipsToBounds = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+    }
+    
+    func configure(with string: String){
+        
+    }
 }
