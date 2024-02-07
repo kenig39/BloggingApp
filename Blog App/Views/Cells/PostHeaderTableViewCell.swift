df73ecc51a349c8936826bed217e4fa856cd0a69
kenig39 <96082144+kenig39@users.noreply.github.com>
2024-02-07 22:24:43 +0300

    add cells

---
+++ Blog App/Views/Cells/PostHeaderTableViewCell.swift
@@ -0,0 +1,8 @@
+
+import UIKit
+
+class PostHeaderTableViewCell: UITableViewCell {
+
+   
+
+}
---
+++ Blog App/Views/Cells/PostPreviewTableViewTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PostPreviewTableViewTableViewCell.swift
+//  Blog App
+//
+//  Created by Alexander on 07.02.2024.
+//
+
+import UIKit
+
+class PostPreviewTableViewTableViewCell: UITableViewCell {
+
+  
+}
